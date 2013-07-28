28d583257ffcc359016bc618c6a181fb653e4d8c
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-07-29 00:46:32 +0800

    add schedule view skeleton

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('shotFormozWebClientApp')
-  .controller('ScheduleController', function ($scope) {
-    //TODO schedule controller here
-  });
+  .controller('ScheduleController', [
+      "$scope"
+      , "$http"
+      , "user"
+    , function ($scope, $http, user) {
+      $scope.HOURS = [ 5, 6, 7, 8, 9, 10, 11 ];
+
+      $http.get(baseUrl + "/users/" + user.id + "/bands")
+        .success(function(bands) {
+          console.log(bands);
+          $scope.bands = bands;
+        });
+  }]);
---
+++ app/scripts/models/user.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('shotFormozWebClientApp')
+  .value('user', {
+    //TODO hard-code for now
+    id: 100
+  });
