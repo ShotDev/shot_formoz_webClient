e05914caaa59cde639d0cb0b1f3cf8f3767ed740
gustavHsiung <knightgustav@gmail.com>
2013-07-30 20:53:00 +0800

    Update app,js 1.init Facebook while app.run 2.Add Facebook service.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('shotFormozWebClientApp', [])
-  .config(function ($routeProvider) {
+var app = angular.module('shotFormozWebClientApp', [])
+  .config(function ($routeProvider,$locationProvider) {
     $routeProvider
       .when("/login", {
         templateUrl: "views/login.html"
@@ -18,4 +18,63 @@ angular.module('shotFormozWebClientApp', [])
       .otherwise({
         redirectTo: '/bands'
       });
+    $locationProvider.html5Mode(true);  
   });
+
+app.run(function ($rootScope, Facebook) {
+    // Additional JS functions here
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '591995264185706', // App ID
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+});
+app.factory('Facebook', function($rootScope, $q) {
+
+    return {
+      login: function() {
+
+        var resp = $q.defer();
+
+        FB.login(function(response) {
+          setTimeout(function() {
+            $rootScope.$apply(function() {
+              resp.resolve(response.authResponse);
+            });
+          },1);
+        });
+
+        $rootScope.Facebook.token = resp.promise;
+
+
+      },
+      logout: function() {
+
+        var resp = $q.defer();
+
+        FB.logout(function(response) {
+          setTimeout(function() {
+            $rootScope.$apply(function() {
+              resp.resolve(response.authResponse);
+            });
+          },1);
+        });
+
+        $rootScope.Facebook.token = null;   
+
+      }
+    }
+  })
