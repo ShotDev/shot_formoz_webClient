7b1732a42220f5bb9b38f85bff795f6100c04d93
gustavHsiung <knightgustav@gmail.com>
2013-08-01 19:37:34 +0800

    Get band list from server. Compare band list with user bands. init Click function.

--- app/scripts/controllers/bands.js
+++ app/scripts/controllers/bands.js
@@ -1,6 +1,37 @@
 'use strict';
 
 angular.module('shotFormozWebClientApp')
-  .controller('BandsController', function ($scope) {
-    //TODO bands controller here
-  });
+  .controller('BandsController', [
+      "$scope"
+      , "$rootScope"
+      , "$location"
+      , "$http"
+      , "Facebook"
+      , "user"
+    , function ($scope, $rootScope, $location, $http,  Facebook, user){
+    	$scope.init = function () {
+    	  if(user){
+    	   	$http.get('/bands').success(function (allBands) {
+		   	    console.log('get bands from server',allBands);
+		   	    user.bands.forEach( function (band){
+		   	      var index = allBands.indexOf(band);
+		   	      if(index >=0 )
+		   	      	allBands[index].selected = 1;
+		   	    })
+                $scope.bands = allBands;
+            });
+    	  }else{
+   	      	$location.path('/login');
+   	      }	
+		   
+		};
+		$scope.bandClick = function (band){
+			
+			if(band.selected)
+				band.selected = -1;
+			else
+				band.selected = 1;
+			console.log('band', band, band.selected?'band selected':'band not selected'); 
+		};
+    	
+ }]);
